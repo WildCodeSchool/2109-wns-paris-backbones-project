3c8b868c056582c06cae265324782dd583984c62
Nathanaël Labreuil <nathanael.labreuil@gmail.com>
2021-10-28 14:58:52 +0200

    Init project and front

---
+++ front/src/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	useQuery,
+	gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "https://48p1r2roz4.sse.codesandbox.io",
+	cache: new InMemoryCache(),
+});
+
+client
+	.query({
+		query: gql`
+			query GetRates {
+				rates(currency: "USD") {
+					currency
+				}
+			}
+		`,
+	})
+	.then((result) => console.log(result));
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
