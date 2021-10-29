1296d40c730ec4fc3293219d9b10be848afaf341
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-10-29 14:35:05 +0200

    create user and role model

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,26 +1,35 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { CustomUser } from "./entity/User";
+import { BackBonesUser } from "./entity/User";
+import { Role } from "./entity/Role";
 // import { ApolloServer } from "apollo-server";
-// import { BookResolver } from "./resolvers/BookResolver.ts";
 
 createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new CustomUser();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+	.then(async (connection) => {
+		console.log("Inserting a new Role into the database...");
+		const role = new Role();
+		role.title = "CTO";
+		await connection.manager.save(role);
+		console.log("Saved a new role with id: " + role.id);
+		console.log("Inserting a new user into the database...");
+		const user = new BackBonesUser();
+		user.firstName = "Thomas";
+		user.lastName = "Brouillet";
+		user.email = "thomas@thomas.com";
+		user.role = role;
+		await connection.manager.save(user);
+		console.log("Saved a new user with id: " + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(CustomUser);
-    console.log("Loaded users: ", users);
+		console.log("Loading users from the database...");
+		const users = await connection.manager.find(BackBonesUser);
+		console.log("Loaded users: ", users);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-  })
-  .catch((error) => console.log(error));
+		console.log(
+			"Here you can setup and run express/koa/any other framework."
+		);
+		console.log(user.role);
+	})
+	.catch((error) => console.log(error));
 
 // async function main() {
 //   const connection = await createConnection();
