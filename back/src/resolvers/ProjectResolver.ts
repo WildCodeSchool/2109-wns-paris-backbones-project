2265383708965fd9eee344f35ff7435e4cb70be4
lfremy <laurafremy19@gmail.com>
2021-11-24 12:04:28 +0100

    Mutations Update Create  & GetProjectbyId

--- back/src/resolvers/ProjectResolver.ts
+++ back/src/resolvers/ProjectResolver.ts
@@ -1,17 +1,70 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
 import { Project } from '../entities/Project';
+import { CreateProjectInput } from "../inputs/ProjectInput";
 
 @Resolver()
 export class ProjectResolver {
     // READ
     @Query(() => [Project])
-
     async getProjects() {
         try {
-            return Project.find()
+            return Project.find();
         }
         catch (error) {
             console.log(error)
         }
+    }
+
+    @Query(() => Project)
+    async getProjectById(@Arg("ProjectId") id: number) {
+        try {
+            return Project.findOne(id)
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    // CREATE
+    @Mutation(() => Project)
+    async addProject(@Arg("CreateProjectInput") CreateProjectInput: CreateProjectInput) {
+        let newProjectId = 0;
+        try {
+            await Project.create(CreateProjectInput)
+                .save()
+                .then((result) => {
+                    if (result.id) {
+                        newProjectId = result.id;
+                        console.log("Succesfully create: ", result);
+                    } else {
+                        console.log("ERROR: We can't create project", result);
+                    }
+                });
+        } catch (error) {
+            console.log(error);
+        }
+        return await Project.findOne(newProjectId);
+    }
+
+
+    //UPDATE
+    @Mutation(() => Project)
+    async updateProject(
+        @Arg("projectId") projectId: number,
+        @Arg("CreateProjectInput") CreateProjectInput: CreateProjectInput
+    ) {
+        try {
+            await Project.update(projectId, CreateProjectInput).then((result) => {
+                if (result) {
+                    console.log("Succesfully update: ", result);
+                } else {
+                    console.log("ERROR: We can't update this Project", result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        return await Project.findOne(projectId);
     }
 }
