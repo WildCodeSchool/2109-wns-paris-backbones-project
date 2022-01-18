fcfa5c2a5fe7b544626e97d195b712565a0f094a
Nathanaël Labreuil <nathanael.labreuil@gmail.com>
2022-01-18 12:02:52 +0100

    chore: Update tailwind config for 3.0

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
