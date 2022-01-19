93c8268925313de36f72d19c558a2ff46c5c5d1a
Thomas Brouillet <thomasbrouillet@gmail.com>
2022-01-19 17:24:47 +0100

    adding polling in ts config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		host: "0.0.0.0"
+		host: "0.0.0.0",
+		watch: {
+			usePolling: true
+		}
 	}
 });
