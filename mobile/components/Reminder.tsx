0c60f18292847a243a0a65f0d9039613de32a286
Thomas Brouillet <thomasbrouillet@gmail.com>
2022-02-22 14:10:47 +0100

    reminder without puce

--- mobile/components/Reminder.tsx
+++ mobile/components/Reminder.tsx
@@ -3,6 +3,7 @@ import { gql, useQuery } from "@apollo/client";
 import { FlatList, TouchableHighlight, View } from "react-native";
 import tw from "../lib/tailwind";
 import GradientWrapper from "./GradientWrapper";
+import moment from "moment";
 
 export const GET_USER_BY_ID = gql`
   query GetUserById($userId: Float!) {
@@ -28,21 +29,39 @@ const Reminder = () => {
   if (loading) {
     return <Text>LOADING</Text>;
   } else {
+    console.log(data);
     return (
       <GradientWrapper
         gradientName={"primary-linear"}
-        style={tw`p-2 max-w-sm rounded-lg`}
+        style={tw`p-2 max-w-sm rounded-lg w-5/6`}
       >
-        <Text
-          style={tw`mb-2 text-2xl font-main-bold text-light-light tracking-tight`}
-        >
-          Reminder
-        </Text>
-        <Text
-          style={tw`mb-2 text-2xl font-main-extralight tracking-tight text-light-light`}
-        >
-          hello {data.getUserById.firstName}
-        </Text>
+        <View style={tw` flex-row justify-between`}>
+          <Text style={tw`mb-2 text-2xl font-main-bold text-light-light`}>
+            Reminder
+          </Text>
+          <Text style={tw`mb-2 text-2xl font-main-light text-light-light`}>
+            {moment().format("ddd DD MMM")}
+          </Text>
+        </View>
+        <View style={tw`w-5/6`}>
+          <FlatList
+            horizontal
+            data={data.getUserById.tasks.slice(0, 5)}
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            legacyImplementation={false}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={tw`pr-32 my-10`}>
+                <Text
+                  style={tw`mb-2 text-2xl font-main-light text-light-light`}
+                >
+                  {item.title}
+                </Text>
+              </View>
+            )}
+          />
+        </View>
       </GradientWrapper>
     );
   }
