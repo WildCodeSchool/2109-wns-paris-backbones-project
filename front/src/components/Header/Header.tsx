35780674f6d3f3b2310c60de09acfe6fce7d1e42
Thomas Brouillet <thomasbrouillet@gmail.com>
2022-04-29 16:09:31 +0200

    fix for fun

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import NavLink from "../NavLink/NavLink";
-import { IconHome, IconTasks } from "../Icons/Icons";
-import HomeIcon from '@material-ui/icons/Home'
-import CheckBoxIcon from '@material-ui/icons/CheckBox'
-import CreateNewFolderIcon from '@material-ui/icons/CreateNewFolder'
-import PersonIcon from '@material-ui/icons/Person'
+import HomeIcon from "@material-ui/icons/Home";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import CreateNewFolderIcon from "@material-ui/icons/CreateNewFolder";
+import PersonIcon from "@material-ui/icons/Person";
 
 function Header() {
 	const routes = [
 		{
 			href: "/",
 			label: "Home",
-			icon: <HomeIcon/>,
+			icon: <HomeIcon />,
 		},
 		{
 			href: "/tasks",
 			label: "Tâches",
-			icon: <CheckBoxIcon/>,
+			icon: <CheckBoxIcon />,
 		},
 		{
 			href: "/projets",
@@ -26,27 +25,21 @@ function Header() {
 		{
 			href: "/account",
 			label: "Profile",
-			icon: <IconTasks />,
-		},
-	];
-	return (
-		<header className="flex items-center justify-between p-4 bg-dark-darker">
-			<p className="text-xl font-bold text-light-light">TicketApp</p>
 			icon: <PersonIcon />,
-			
 		},
 	];
 	return (
 		<nav className="flex items-center justify-between p-4 mx-4 rounded-md bg-dark-medium">
 			<div className="flex gap-4">
-				<CheckBoxIcon  className="text-primary-darker"/>
+				<CheckBoxIcon className="text-primary-darker" />
 				<article>
-					<p className="text-2xl font-bold text-primary-darker">TicketApp</p>
+					<p className="text-2xl font-bold text-primary-darker">
+						TicketApp
+					</p>
 					<p className="text-sm text-light-light">Check your boxes</p>
 				</article>
-				
 			</div>
-			
+
 			<ul className="flex gap-10">
 				{routes.map((route, index) => (
 					<NavLink key={index} route={route} />
