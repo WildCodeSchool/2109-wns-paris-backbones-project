b9977a9f5bd222b7750c3ef2f53522f2d6ae921e
lfremy <laurafremy19@gmail.com>
2022-04-29 14:15:34 +0200

    change icons from material ui and Icons Folder deleted

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,39 +1,46 @@
 import React from "react";
 import NavLink from "../NavLink/NavLink";
 import { IconHome, IconTasks } from "../Icons/Icons";
+import HomeIcon from '@material-ui/icons/Home'
+import CheckBoxIcon from '@material-ui/icons/CheckBox'
+import CreateNewFolderIcon from '@material-ui/icons/CreateNewFolder'
+import PersonIcon from '@material-ui/icons/Person'
+
+
 
 function Header() {
 	const routes = [
 		{
 			href: "/",
 			label: "Home",
-			icon: <IconHome />,
+			icon: <HomeIcon/>,
 		},
 		{
 			href: "/tasks",
 			label: "Tâches",
-			icon: <IconTasks />,
+			icon: <CheckBoxIcon/>,
 		},
 		{
 			href: "/projets",
 			label: "Projects",
-			icon: <IconTasks />,
+			icon: <CreateNewFolderIcon />,
 		},
 		{
 			href: "/account",
 			label: "Profile",
-			icon: <IconTasks />,
+			icon: <PersonIcon />,
+			
 		},
 	];
 	return (
-		<header className="flex items-center justify-between p-4 bg-dark-darker">
+		<nav className="flex items-center justify-between p-4 bg-dark-darker">
 			<p className="text-xl font-bold text-light-light">TicketApp</p>
 			<ul className="flex gap-10">
 				{routes.map((route, index) => (
 					<NavLink key={index} route={route} />
 				))}
 			</ul>
-		</header>
+		</nav>
 	);
 }
 
--- front/src/components/NavLink/NavLink.tsx
+++ front/src/components/NavLink/NavLink.tsx
@@ -9,7 +9,7 @@ export interface Route {
 function NavLink({ route }: { route: Route }) {
 	return (
 		<div className="flex items-center justify-center">
-			<div className="text-green-500">{route.icon}</div>
+			<div className="text-primary-darker">{route.icon}</div>
 			<p className="text-white">{route.label}</p>
 		</div>
 	);
