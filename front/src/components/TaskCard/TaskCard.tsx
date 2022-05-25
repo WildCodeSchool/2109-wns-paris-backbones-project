4aac24d58087db440d3fe321de1a600e3862a79d
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-05-25 11:24:23 +0200

    <TaskCard> First implement DialogBox logic
    Co-Authored-By: lfremy <75318984+lfremy@users.noreply.github.com>

--- front/src/components/TaskCard/TaskCard.tsx
+++ front/src/components/TaskCard/TaskCard.tsx
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import { Task } from "../types";
 import UserBadge from "../UserBadge/UserBadge";
+import { DialogBox } from "../utils/DialogBox";
 
 interface TaskCardProps {
-	task: Task;
+	task: Task
 }
 
 const TaskCard = ({ task }: TaskCardProps) => {
 	const { title, users } = task;
+
+	let [isOpen, setIsOpen] = useState(false)
+
+	const openDialog = () => {
+		setIsOpen(true)
+	};
+
 	return (
-		<div className="task-holder flex flex-row w-full items-center justify-start px-2 py-1 my-3 bg-light-dark dark:bg-dark-dark rounded-3xl">
+		<div onClick={() => openDialog()} className="task-holder flex flex-row w-full items-center justify-start px-2 py-1 my-3 bg-light-dark dark:bg-dark-dark rounded-3xl">
 			<div className="icon"></div>
 			<span className="task-title w-8/12 px-1 flex-nowrap">{title}</span>
 			{users && (
@@ -23,6 +32,7 @@ const TaskCard = ({ task }: TaskCardProps) => {
 					))}
 				</ul>
 			)}
+			{ isOpen && <DialogBox/>}
 		</div>
 	);
 };
