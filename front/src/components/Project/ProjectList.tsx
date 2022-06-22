7ec57579e170b31e09af3a4a6e8d4984e5f52c21
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-06-22 12:01:47 +0200

    Update ProjectList.tsx

--- front/src/components/Project/ProjectList.tsx
+++ front/src/components/Project/ProjectList.tsx
@@ -1,5 +1,6 @@
 import { Project } from "../types";
 import ProjectCard from "./ProjectCard";
+import AddIcon from "@mui/icons-material/Add";
 
 interface ProjectListProps {
 	projects: Project[];
@@ -9,6 +10,10 @@ function ProjectList({ projects }: ProjectListProps) {
 	return (
 		<div>
 			<h1 className="text-2xl text-center">Projects</h1>
+			<span className="flex align-center">
+				<AddIcon className="text-primary-medium flex justify-self-end"/>
+				Add a new project
+			</span>
 			<div className="flex flex-wrap justify-center">
 				{projects.map((project) => (
 					<ProjectCard key={project.id} project={project} />
